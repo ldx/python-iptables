fd1540105c19d10da84dde21d5dd4f6a12bfac1f
ldx <ldx@nilvec.com>
2013-01-25 19:00:14 +0100

    Setup.py cosmetics.

--- setup.py
+++ setup.py
@@ -9,22 +9,22 @@ execfile("iptc/version.py")
 
 # build/install python-iptables
 setup(
-    name                = __pkgname__,
-    version             = __version__,
-    description         = "Python bindings for iptables",
-    author              = "Nilvec",
-    author_email        = "nilvec@nilvec.com",
-    url                 = "http://nilvec.com/",
-    packages            = ["iptc"],
-    package_dir         = {"iptc" : "iptc"},
-    ext_modules         = [Extension("libxtwrapper",
-        ["libxtwrapper/wrapper.c"])], #"libxtwrapper/xtoptions.c"])],
-    classifiers         = [
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: Apache License, Version 2.0",
-        "Natural Language :: English",
-        "Topic :: Networking",
-    ],
-    license    = "Apache License, Version 2.0",
-)
+        name                = __pkgname__,
+        version             = __version__,
+        description         = "Python bindings for iptables",
+        author              = "Nilvec",
+        author_email        = "nilvec@nilvec.com",
+        url                 = "http://nilvec.com/",
+        packages            = ["iptc"],
+        package_dir         = {"iptc" : "iptc"},
+        ext_modules         = [Extension("libxtwrapper",
+            ["libxtwrapper/wrapper.c"])],
+        classifiers         = [
+            "Development Status :: 4 - Beta",
+            "Intended Audience :: Developers",
+            "License :: OSI Approved :: Apache License, Version 2.0",
+            "Natural Language :: English",
+            "Topic :: Networking",
+            ],
+        license    = "Apache License, Version 2.0",
+        )
