b10fcb45f71cc1a9474c268777151ec726092663
ldx <ldx@nilvec.com>
2013-02-06 23:23:01 +0100

    Move kernel loading stuff to util.py.

--- iptc/ip4tc.py
+++ iptc/ip4tc.py
@@ -9,35 +9,11 @@ import weakref
 import ctypes.util
 
 from xtables import XT_INV_PROTO, NFPROTO_IPV4, XTF_TRY_LOAD, XTablesError, xtables, xt_align, xt_counters, xt_entry_target, xt_entry_match
+from util import load_kernel
 
 __all__ = ["Table", "Chain", "Rule", "Match", "Target", "Policy", "IPTCError",
            "POLICY_ACCEPT", "POLICY_DROP", "POLICY_QUEUE", "POLICY_RETURN",
            "TABLE_FILTER", "TABLE_NAT", "TABLE_MANGLE", "TABLES"]
-
-from subprocess import Popen, PIPE
-
-def _insert_ko(modprobe, modname):
-    p = Popen([modprobe, modname], stderr=PIPE)
-    p.wait()
-    return (p.returncode, p.stderr.read(1024))
-
-def _load_ko(modname):
-    # this will return the full path for the modprobe binary
-    proc = open("/proc/sys/kernel/modprobe")
-    modprobe = proc.read(1024)
-    if modprobe[len(modprobe) - 1] == '\n':
-        modprobe = modprobe[:len(modprobe) - 1]
-    return _insert_ko(modprobe, modname)
-
-# First load the kernel module.  If it is already loaded modprobe will just
-# return with 0.
-_rc, _err = _load_ko("ip_tables")
-if _rc:
-    if not _err:
-        _err = "Failed to load the ip_tables kernel module."
-    if _err[len(_err) - 1] == "\n":
-        _err = _err[:len(_err) - 1]
-    raise Exception(_err)
 
 _IFNAMSIZ = 16
 
