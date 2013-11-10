2bf3c71a87e4f5926b63a66aa456ac9b04d5eaf5
ldx <ldx@nilvec.com>
2013-11-10 14:25:47 +0100

    Remove accidentally committed files.

--- x.py
+++
@@ -1,8 +0,0 @@
-import iptc
-
-table = iptc.Table(iptc.Table.FILTER)
-chain = iptc.Chain(table, 'mychain')
-rule = iptc.Rule()
-
-for idx, r in enumerate(chain.rules, 1):
-    print idx, r.src, r.dst
