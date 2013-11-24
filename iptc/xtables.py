af51e8a2442577ae8fc386d697a152fd877a5a7b
ldx <ldx@nilvec.com>
2013-11-24 12:07:58 +0100

    Fix #45.

--- iptc/xtables.py
+++ iptc/xtables.py
@@ -684,6 +684,7 @@ _xtables_libdir = os.getenv("XTABLES_LIBDIR")
 if _xtables_libdir is None:
     import os.path
     for xtdir in ["/lib/xtables", "/usr/lib/xtables", "/usr/lib/iptables",
+                  "/usr/lib64/xtables", "/usr/lib64/iptables",
                   "/usr/local/lib/xtables"]:
         if os.path.isdir(xtdir):
             _xtables_libdir = xtdir
