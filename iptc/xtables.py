b6a1c4c91a48dc527ce8d003c11e536fd3683802
Pajaro_xdd <pabloalvarezcordoba@protonmail.ch>
2017-08-31 01:43:47 +0200

    modified:   iptc/xtables.py 	Add new xtdir to fix error on Debian

--- iptc/xtables.py
+++ iptc/xtables.py
@@ -804,7 +804,7 @@ if _xtables_libdir is None:
     import os.path
     for xtdir in ["/lib/xtables", "/lib64/xtables", "/usr/lib/xtables",
                   "/usr/lib/iptables", "/usr/lib64/xtables",
-                  "/usr/lib64/iptables", "/usr/local/lib/xtables"]:
+                  "/usr/lib64/iptables", "/usr/local/lib/xtables", "/usr/lib/x86_64-linux-gnu/xtables"]:
         if os.path.isdir(xtdir):
             _xtables_libdir = xtdir
             break
